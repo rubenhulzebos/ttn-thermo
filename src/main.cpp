7df608e604dcfb00aae4daaaaea2852d43daa256
rubenhulzebos <ruben@hulzebos.com>
2020-05-01 15:08:35 +0200

    DS1820 added

--- src/main.cpp
+++ src/main.cpp
@@ -34,8 +34,8 @@
 /*
  *  MAX6675 Module   ==>   Arduino
  *    CS             ==>     D10
- *    SO             ==>     D12
- *    SCK            ==>     D13
+ *    SO             ==>     D12 -> D9
+ *    SCK            ==>     D13 -> D8
  *    Vcc            ==>     Vcc (5v OK)
  *    Gnd            ==>     Gnd
  * */
@@ -43,32 +43,46 @@
 #include <lmic.h>
 #include <hal/hal.h>
 #include <SPI.h>
-//#include <SoftwareSerial.h>
+#include <OneWire.h>
+#include <DallasTemperature.h>
 
-#include <SPI.h>
+// Data wire is plugged into port 2 on the Arduino
+#define ONE_WIRE_BUS 9
+#define DS1820vcc 8
+#define DS1820gnd 7
+
+// Setup a oneWire instance to communicate with any OneWire devices (not just Maxim/Dallas temperature ICs)
+OneWire oneWire(ONE_WIRE_BUS);
+
+// Pass our oneWire reference to Dallas Temperature. 
+DallasTemperature sensors(&oneWire);
+
+// arrays to hold device addresses
+DeviceAddress DS1820addr;
 
 #define MAX6675_CS   10
-#define MAX6675_SO   12
-#define MAX6675_SCK  13
+#define MAX6675_SO   9
+#define MAX6675_SCK  8
 
-uint8_t coords[12];
+//uint8_t coords[12];
+uint8_t temperatureData[2];
 
 // This EUI must be in little-endian format, so least-significant-byte
 // first. When copying an EUI from ttnctl output, this means to reverse
 // the bytes. For TTN issued EUIs the last bytes should be 0xD5, 0xB3,
 // 0x70.
-static const u1_t PROGMEM APPEUI[8] = {0x0C, 0xC5, 0x02, 0xD0, 0x7E, 0xD5, 0xB3, 0x70};
+static const u1_t PROGMEM APPEUI[8] = { 0x0F, 0xE7, 0x02, 0xD0, 0x7E, 0xD5, 0xB3, 0x70 };
 void os_getArtEui(u1_t *buf) { memcpy_P(buf, APPEUI, 8); }
 
 // This should also be in little endian format, see above.
-static const u1_t PROGMEM DEVEUI[8] = {0x05, 0xA1, 0x3B, 0xFC, 0xAF, 0x09, 0xE2, 0x06};
+static const u1_t PROGMEM DEVEUI[8] = { 0x01, 0x78, 0xC7, 0x2A, 0x68, 0xED, 0x83, 0xF0 };
 void os_getDevEui(u1_t *buf) { memcpy_P(buf, DEVEUI, 8); }
 
 // This key should be in big endian format (or, since it is not really a
 // number but a block of memory, endianness does not really apply). In
 // practice, a key taken from ttnctl can be copied as-is.
 // The key shown here is the semtech default key.
-static const u1_t PROGMEM APPKEY[16] = {0x9E, 0x1B, 0xAD, 0xA4, 0x64, 0x94, 0x14, 0x54, 0x49, 0x07, 0x07, 0xF6, 0x50, 0x41, 0x3D, 0xE7};
+static const u1_t PROGMEM APPKEY[16] = { 0xAF, 0x84, 0xC0, 0x98, 0x5E, 0xB7, 0xF6, 0xA2, 0xC0, 0x69, 0x1A, 0x0F, 0x1F, 0xCC, 0xE0, 0x64 };
 void os_getDevKey(u1_t *buf) { memcpy_P(buf, APPKEY, 16); }
 
 //static uint8_t mydata[] = "Hello, world!";
@@ -120,10 +134,77 @@ double readThermocouple() {
   return v*0.25;
 }
 
+// function to print a device address
+void printDS1820Address(DeviceAddress deviceAddress)
+{
+  for (uint8_t i = 0; i < 8; i++)
+  {
+    if (deviceAddress[i] < 16) Serial.print("0");
+    Serial.print(deviceAddress[i], HEX);
+  }
+}
+
+// main function to print information about a device
+void printData(DeviceAddress deviceAddress)
+{
+  Serial.print("Device Address: ");
+  printDS1820Address(deviceAddress);
+  Serial.println(" ");
+}
+
+void powerOnDS1820(void)
+{
+  digitalWrite(DS1820vcc, HIGH);
+  digitalWrite(DS1820gnd, LOW);
+}
+
+void powerOffDS1820(void)
+{
+  digitalWrite(DS1820vcc, LOW);
+  digitalWrite(DS1820gnd, LOW);
+}
+
 void get_temp()
 {
-  Serial.print(readThermocouple());
+  uint16_t thermodata = readThermocouple();
+  Serial.print(thermodata);
   Serial.println('c');
+
+  temperatureData[0] = (uint8_t)thermodata;
+  temperatureData[1] = (uint8_t)(thermodata >> 8);
+}
+
+void get_tempDS1820()
+{
+  Serial.print("Requesting temperatures...");
+  powerOnDS1820();
+  sensors.requestTemperatures();
+  Serial.println("DONE");
+
+ 
+
+  float tempC = sensors.getTempC(DS1820addr);
+  Serial.print("Temp C: ");
+  Serial.println(tempC);
+
+  tempC = tempC * 100;
+
+  uint16_t tempC16;
+  tempC16 = (uint16_t)tempC;
+  
+  Serial.print("tempC16: ");
+  Serial.println(tempC16);
+
+  temperatureData[0] = (uint8_t)tempC16;
+  temperatureData[1] = (uint8_t)(tempC16 >> 8);
+
+  Serial.print("Byte 0: ");
+  Serial.println(temperatureData[0], HEX);
+
+  Serial.print("Byte 1: ");
+  Serial.println(temperatureData[1], HEX);
+
+  powerOffDS1820();
 }
 
 void do_send(osjob_t *j)
@@ -136,10 +217,11 @@ void do_send(osjob_t *j)
     else
     {
         // Prepare upstream data transmission at the next possible time.
-        get_temp();
+        //get_temp();
+        get_tempDS1820();
         //get_coords();
         //LMIC_setTxData2(1, (uint8_t *)coords, sizeof(coords), 0);
-        LMIC_setTxData2(1, (uint8_t *)coords, sizeof(coords), 0);
+        LMIC_setTxData2(1, (uint8_t *)temperatureData, sizeof(temperatureData), 0);
         Serial.println(F("Packet queued"));
     }
     // Next TX is scheduled after TX_COMPLETE event.
@@ -233,12 +315,40 @@ void onEvent(ev_t ev)
     }
 }
 
+
+
+void setupDS1820()
+{
+  pinMode(DS1820vcc, OUTPUT); 
+  pinMode(DS1820gnd, OUTPUT); 
+  powerOnDS1820();
+
+  // Start up the library
+  sensors.begin();
+  
+  // locate devices on the bus
+  Serial.print("Found ");
+  Serial.print(sensors.getDeviceCount(), DEC);
+  Serial.println(" devices.");
+
+  // search for devices on the bus and assign based on an index.
+  if (!sensors.getAddress(DS1820addr, 0)) Serial.println("Unable to find address for Device 0"); 
+  
+  // show the addresses we found on the bus
+  Serial.print("Device 0 Address: ");
+  printDS1820Address(DS1820addr);
+  Serial.println();
+  powerOffDS1820();
+}
+
 void setup()
 {
     Serial.begin(115200);
     Serial.println(F("Starting"));
 
-    delay(5000);
+    setupDS1820();
+
+    delay(2000);
     
 #ifdef VCC_ENABLE
         // For Pinoccio Scout boards
